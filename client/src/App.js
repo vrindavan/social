82fc6d7f71a145f2a8ef285f1643926d4dea3736
Amay <brajbliss@gmail.com>
2022-11-06 02:22:26 +0530

    register/login

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Route,
+	Outlet,
+	Navigate,
+} from 'react-router-dom';
+import LeftBar from './components/leftBar/LeftBar';
+import Navbar from './components/navbar/Navbar';
+import RightBar from './components/rightBar/RightBar';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Profile from './pages/profile/Profile';
+import Home from './pages/home/Home';
 
 const App = () => {
-	return <div>App</div>;
+	const currentUser = false;
+
+	const Layout = () => {
+		return (
+			<div>
+				<Navbar />
+				<div style={{ display: 'flex' }}>
+					<LeftBar />
+					<Outlet />
+					<RightBar />
+				</div>
+			</div>
+		);
+	};
+
+	const ProtectedRoute = ({ children }) => {
+		if (!currentUser) return <Navigate to='/login' />;
+		return children;
+	};
+
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: (
+				<ProtectedRoute>
+					<Layout />
+				</ProtectedRoute>
+			),
+			children: [
+				{
+					path: '/',
+					element: <Home />,
+				},
+				{
+					path: '/profile/:id',
+					element: <Profile />,
+				},
+			],
+		},
+		{
+			path: '/login',
+			element: <Login />,
+		},
+		{ path: '/register', element: <Register /> },
+	]);
+
+	return (
+		<div>
+			<RouterProvider router={router} />
+		</div>
+	);
 };
 
 export default App;
